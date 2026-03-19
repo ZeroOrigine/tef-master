f2f8c4af47527f5bd421b60cbceec91cc4cae3cb
Jagdish Lade <cajagdishlade@gmail.com>
2026-03-19 02:45:54 -0400

    fix: update Meta Pixel ID to 2313815159111295

--- js/analytics.js
+++ js/analytics.js
@@ -3,7 +3,7 @@
 
 (function() {
   var GA_ID = 'G-QPJM300PZK';
-  var FB_PIXEL_ID = '1573840330386741';
+  var FB_PIXEL_ID = '2313815159111295';
 
   // ---- Google Analytics 4 ----
   var gaScript = document.createElement('script');
